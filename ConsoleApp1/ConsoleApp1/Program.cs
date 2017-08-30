13de53cab76b8986d9c4f56990807e2425fd718f
Jiahui Chen <jiahui.k.chen@gmail.com>
2017-08-30 11:04:08 -0600

    Created Lab 1 Solution

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    ///I love GIT
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Hello World");
+            Console.Read();
+        }
+    }
+}
