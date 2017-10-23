2b9d5b8c238d1ae85283ee6cef80b3d0317f88b1
Mitch Talmadge <u1031378@utah.edu>
2017-10-23 11:19:56 -0600

    Allow multiple spreadsheets to be opened

--- PS6/SpreadsheetGUI/Program.cs
+++ PS6/SpreadsheetGUI/Program.cs
@@ -6,17 +6,65 @@ using System.Windows.Forms;
 
 namespace SpreadsheetGUI
 {
+    internal class SpreadsheetApplicationContext : ApplicationContext
+    {
+        private static SpreadsheetApplicationContext _instance;
+
+        /// <summary>
+        /// The singleton instance of this application context.
+        /// </summary>
+        internal static SpreadsheetApplicationContext Instance =>
+            _instance ?? (_instance = new SpreadsheetApplicationContext());
+
+        /// <summary>
+        /// The number of currently open spreadsheets.
+        /// </summary>
+        private int OpenSpreadsheets { get; set; }
+
+        /// <summary>
+        /// Private constructor to prevent multiple instantiations of the singleton context.
+        /// </summary>
+        private SpreadsheetApplicationContext()
+        {
+        }
+
+        /// <summary>
+        /// Creates and displays a new spreadsheet in the current application context.
+        /// </summary>
+        internal void OpenSpreadsheet()
+        {
+            // Create a spreadsheet
+            var spreadsheet = new SpreadsheetForm();
+            OpenSpreadsheets++;
+
+            // Add a listener for when the spreadsheet form is closed.
+            // This also shuts down the application when all spreadsheets are closed.
+            spreadsheet.FormClosed += (o, e) =>
+            {
+                if (--OpenSpreadsheets <= 0) ExitThread();
+            };
+
+            // Show the new spreadsheet.
+            spreadsheet.Show();
+        }
+    }
+
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Creates an application context and opens a single spreadsheet.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SpreadsheetForm());
+
+            // Open one spreadsheet.
+            SpreadsheetApplicationContext.Instance.OpenSpreadsheet();
+
+            // Run the singleton Spreadsheet Application Context
+            Application.Run(SpreadsheetApplicationContext.Instance);
         }
     }
 }
