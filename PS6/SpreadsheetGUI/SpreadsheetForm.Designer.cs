6ac87596f26b8a1747c04b454e376df229d84200
Mitch Talmadge <u1031378@utah.edu>
2017-10-23 11:04:51 -0600

    Rename Form1 and add spreadsheet panel

---
+++ PS6/SpreadsheetGUI/SpreadsheetForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SpreadsheetGUI
+{
+    partial class SpreadsheetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spreadsheetPanel = new SS.SpreadsheetPanel();
+            this.SuspendLayout();
+            // 
+            // spreadsheetPanel
+            // 
+            this.spreadsheetPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spreadsheetPanel.Location = new System.Drawing.Point(0, 0);
+            this.spreadsheetPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.spreadsheetPanel.Name = "spreadsheetPanel";
+            this.spreadsheetPanel.Size = new System.Drawing.Size(2179, 1391);
+            this.spreadsheetPanel.TabIndex = 0;
+            // 
+            // SpreadsheetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2179, 1391);
+            this.Controls.Add(this.spreadsheetPanel);
+            this.Name = "SpreadsheetForm";
+            this.ShowIcon = false;
+            this.Text = "Spreadsheet - Jiahui Chen & Mitch Talmadge";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SS.SpreadsheetPanel spreadsheetPanel;
+    }
+}
+
--- PS6/SpreadsheetGUI/SpreadsheetForm.cs
+++ PS6/SpreadsheetGUI/SpreadsheetForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SpreadsheetGUI
 {
-    public partial class Form1 : Form
+    public partial class SpreadsheetForm : Form
     {
-        public Form1()
+        public SpreadsheetForm()
         {
             InitializeComponent();
         }
