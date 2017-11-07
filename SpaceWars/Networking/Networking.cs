efb0ae58c89a4348d674ac59322cd2a0d527ae9e
Jiahui Chen <jiahui.k.chen@gmail.com>
2017-11-06 17:39:46 -0700

    Networking project structure created.

---
+++ SpaceWars/Networking/Networking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking
+{
+    /// <summary>
+    /// Static class holding static networking methods.
+    /// </summary>
+    public static class Networking
+    {
+    }
+}
