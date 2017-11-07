7f31e5a449b2ff634661c8dc659bcd21c5f8852d
Mitch Talmadge <u1031378@utah.edu>
2017-11-06 21:10:08 -0700

    Improve layout of GameForm

--- SpaceWars/View/GameForm.Designer.cs
+++ SpaceWars/View/GameForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.ConnectButton = new System.Windows.Forms.Button();
+            this.LeaveButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -37,41 +37,41 @@
             // 
             this.tableLayoutPanel.ColumnCount = 2;
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 750F));
-            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 350F));
-            this.tableLayoutPanel.Controls.Add(this.ConnectButton, 0, 1);
-            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel.Controls.Add(this.LeaveButton, 0, 1);
             this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 2;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 750F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel.Size = new System.Drawing.Size(1074, 929);
             this.tableLayoutPanel.TabIndex = 0;
             // 
-            // ConnectButton
+            // LeaveButton
             // 
-            this.ConnectButton.AutoSize = true;
-            this.ConnectButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ConnectButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.tableLayoutPanel.SetColumnSpan(this.ConnectButton, 2);
-            this.ConnectButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ConnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ConnectButton.Font = new System.Drawing.Font("Comic Sans MS", 28.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ConnectButton.ForeColor = System.Drawing.Color.White;
-            this.ConnectButton.Location = new System.Drawing.Point(20, 770);
-            this.ConnectButton.Margin = new System.Windows.Forms.Padding(20);
-            this.ConnectButton.Name = "ConnectButton";
-            this.ConnectButton.Size = new System.Drawing.Size(1060, 160);
-            this.ConnectButton.TabIndex = 5;
-            this.ConnectButton.Text = "Leave Game";
-            this.ConnectButton.UseVisualStyleBackColor = false;
+            this.LeaveButton.AutoSize = true;
+            this.LeaveButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.LeaveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tableLayoutPanel.SetColumnSpan(this.LeaveButton, 2);
+            this.LeaveButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LeaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LeaveButton.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LeaveButton.ForeColor = System.Drawing.Color.White;
+            this.LeaveButton.Location = new System.Drawing.Point(20, 770);
+            this.LeaveButton.Margin = new System.Windows.Forms.Padding(20);
+            this.LeaveButton.Name = "LeaveButton";
+            this.LeaveButton.Size = new System.Drawing.Size(1034, 139);
+            this.LeaveButton.TabIndex = 5;
+            this.LeaveButton.Text = "Leave Game";
+            this.LeaveButton.UseVisualStyleBackColor = false;
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1074, 929);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1162, 983);
             this.Controls.Add(this.tableLayoutPanel);
             this.Name = "GameForm";
             this.Text = "GameForm";
@@ -84,6 +84,6 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
-        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button LeaveButton;
     }
 }
