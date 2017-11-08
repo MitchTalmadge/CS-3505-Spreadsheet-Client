37c84ae69fac7d313e332e6091b34194b088322e
Mitch Talmadge <u1031378@utah.edu>
2017-11-08 01:07:47 -0700

    Begin work on ships

---
+++ SpaceWars/GameComponents/Ship.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceWars
+{
+    public class Ship
+    {
+
+        private void Painter(PaintEventArgs e)
+        {
+            //int shipWidth = 35;
+            /*if (s.GetID() == ...)
+                color = ...;
+            else if (...)
+                color = ...;*/
+            //Rectangle r = new Rectangle(-(shipWidth / 2), -(shipWidth / 2), shipWidth, shipWidth);
+            //e.Graphics.FillRectangle(someBrush, r);
+        }
+
+    }
+}
--- SpaceWars/WorldPanel/WorldPanel.cs
+++ SpaceWars/WorldPanel/WorldPanel.cs
@@ -24,8 +24,6 @@ namespace SpaceWars
             // Draw double border
             e.Graphics.DrawRectangle(Pens.White, 0, 0, ClientSize.Width - 1, ClientSize.Height - 1); // Outer
             e.Graphics.DrawRectangle(Pens.Aqua, 2, 2, ClientSize.Width - 5, ClientSize.Height - 5); // Inner
-
-
         }
     }
 }
