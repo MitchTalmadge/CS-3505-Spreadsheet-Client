b59668c4678e3234abb04fad75d15b6b49cdd3cc
Mitch Talmadge <u1031378@utah.edu>
2017-11-09 16:04:33 -0700

    Add world-space to image-space conversion

--- SpaceWars/WorldPanel/WorldPanel.cs
+++ SpaceWars/WorldPanel/WorldPanel.cs
@@ -13,6 +13,16 @@ namespace SpaceWars
     public sealed class WorldPanel : Panel
     {
         /// <summary>
+        /// The size of the game world, in either direction (width or height).
+        /// The game world is always square.
+        /// </summary>
+        public int WorldSize
+        {
+            get => Size.Height;
+            set => Size = new Size(value, value);
+        }
+
+        /// <summary>
         /// The game components to be drawn when this component is painted.
         /// </summary>
         private IEnumerable<GameComponent> _gameComponents = new GameComponent[0];
@@ -20,6 +30,16 @@ namespace SpaceWars
         public WorldPanel()
         {
             BackColor = Color.Transparent;
+            DoubleBuffered = true;
+        }
+
+        /// <summary>
+        /// Schedules the given game components to be drawn on the next tick.
+        /// </summary>
+        /// <param name="gameComponents">The components to draw, in the order to be drawn.</param>
+        public void DrawGameComponents(IEnumerable<GameComponent> gameComponents)
+        {
+            _gameComponents = gameComponents.ToArray();
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -35,20 +55,31 @@ namespace SpaceWars
             // Draw game components
             foreach (var gameComponent in _gameComponents)
             {
+                // Translate the graphics object so that drawing at (0, 0) will put the component in the correct place.
+                var translation = WorldVectorToImagePoint(gameComponent.Location);
+                e.Graphics.TranslateTransform(translation.X, translation.Y);
+                e.Graphics.RotateTransform(gameComponent.Direction.ToAngle());
+
+                // Draw the component at (0, 0)
                 var imageDetails = gameComponent.GetDrawingDetails();
-                e.Graphics.DrawImage(imageDetails.Item1,
-                    new Rectangle(0, 0, imageDetails.Item2.Width, imageDetails.Item2.Height), imageDetails.Item2,
+                var image = imageDetails.Item1;
+                var cropRegion = imageDetails.Item2;
+                e.Graphics.DrawImage(image, new Rectangle(0, 0, cropRegion.Width, cropRegion.Height), cropRegion,
                     GraphicsUnit.Pixel);
+
+                // Restore the original transformation of the graphics.
+                e.Graphics.ResetTransform();
             }
         }
 
         /// <summary>
-        /// Schedules the given game components to be drawn on the next tick.
+        /// Converts the given world-space based vector to an image-space based point.
         /// </summary>
-        /// <param name="gameComponents">The components to draw, in the order to be drawn.</param>
-        public void DrawGameComponents(IEnumerable<GameComponent> gameComponents)
+        /// <param name="vector">The world-space vector to convert.</param>
+        /// <returns>A new point containing the converted vector coordinates.</returns>
+        private Point WorldVectorToImagePoint(Vector2D vector)
         {
-            _gameComponents = gameComponents.ToArray();
+            return new Point((int) vector.GetX() + WorldSize / 2, (int) vector.GetY() + WorldSize / 2);
         }
     }
 }
