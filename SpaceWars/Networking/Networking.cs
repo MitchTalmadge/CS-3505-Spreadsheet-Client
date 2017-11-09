5c9407dbda7c38aa0d88e2ae4ed32ae3dc003098
Jiahui Chen <jiahui.k.chen@gmail.com>
2017-11-08 22:52:32 -0700

    ConnectToServer, ConnectedToServer, and ReceiveCallback tentatively finished. Fields for SocketState added.

--- SpaceWars/Networking/Networking.cs
+++ SpaceWars/Networking/Networking.cs
@@ -13,7 +13,7 @@ namespace Networking
     /// </summary>
     public static class Networking
     {
-        public delegate void Callback(byte[] data);
+        public delegate void HandleData(byte[] data);
 
         /// <summary>
         /// Attempts to connect to the server via a provided hostname. 
@@ -22,9 +22,9 @@ namespace Networking
         /// <param name="callbackFunction"></param>
         /// <param name="hostname"></param>
         /// <returns></returns>
-        public static Socket ConnectToServer(Callback callbackFunction, string hostname)
+        public static Socket ConnectToServer(HandleData callbackFunction, string hostname)
         {
-            //parsing host address and creating corresponding socket
+            //parsing host address and creating corresponding socket and SocketState
             IPAddress address = IPAddress.Parse(hostname);
             Socket socket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             SocketState socketState = new SocketState(socket, null, callbackFunction);
@@ -43,8 +43,13 @@ namespace Networking
         {
             //stateAsArObject contains a field AsyncState which contains a SocketState object
             SocketState socketState = (SocketState)stateAsArObject.AsyncState;
-            socketState.socket.EndConnect(stateAsArObject);
-            socketState.callbackFunction(new byte[0]);
+            Socket socket = socketState.socket;
+            socket.EndConnect(stateAsArObject);
+
+            // Wait for data to come from the server
+            // We pass the last argument (state) so that the callback knows which connection data was received on
+            socket.BeginReceive(socketState.dataBuffer, 0, socketState.dataBuffer.Length, SocketFlags.None, ReceiveCallback, socketState);
+            socketState.handleData(socketState.dataBuffer);///???
         }
 
         public static void GetData(SocketState state)
@@ -52,9 +57,37 @@ namespace Networking
 
         }
 
+        /// <summary>
+        ///  Called by the OS when new data arrives. This method should check to see how much data has arrived.
+        ///  If 0, the connection has been closed (presumably by the server). On greater than zero data, 
+        ///  this method should get the SocketState object out of the IAsyncResult (just as above in 2), 
+        ///  and call the callMe provided in the SocketState.
+        /// </summary>
+        /// <param name="stateAsArObject"></param>
         public static void ReceiveCallback(IAsyncResult stateAsArObject)
         {
-            return;
+            // Get the SocketState associated with the received data
+            SocketState state = (SocketState)stateAsArObject.AsyncState;
+
+            int numBytes = state.socket.EndReceive(stateAsArObject);
+
+            if (numBytes > 0)
+            {
+                // Convert the raw bytes to a string
+                string data = Encoding.UTF8.GetString(state.dataBuffer, 0, numBytes);
+
+                // Append the data to a growable buffer.
+                // We don't know how much data arrived, or if we have an incomplete message.
+                state.sb.Append(data);
+
+
+                // calling delegate on data???
+                state.handleData(state.dataBuffer);
+
+                // Wait for more data from the server. This creates an "event loop".
+                // ReceiveCallback will be invoked every time new data is available on the socket.
+                state.socket.BeginReceive(state.dataBuffer, 0, state.dataBuffer.Length, SocketFlags.None, ReceiveCallback, state);
+            }
         }
 
         public static void Send(Socket socket, String data)
