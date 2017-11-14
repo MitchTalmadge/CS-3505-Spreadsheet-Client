2eef5db723539388a7d0ea0627592a8774500e09
Jiahui Chen <jiahui.k.chen@gmail.com>
2017-11-14 00:55:57 -0700

    Added star to resources reference.

--- SpaceWars/GameComponents/Properties/Resources.Designer.cs
+++ SpaceWars/GameComponents/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace SpaceWars.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap star {
+            get {
+                object obj = ResourceManager.GetObject("star", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
