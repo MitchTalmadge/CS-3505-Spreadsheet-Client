e375553e6ea0b29d42c8b2702bee7344c56f57d9
Jiahui Chen <jiahui.k.chen@gmail.com>
2017-11-13 19:30:05 -0700

    Projectile component started.

---
+++ SpaceWars/GameComponents/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SpaceWars
+{
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Represents a Projectile.
+    /// </summary>
+    /// <authors>Jiahui Chen, Mitch Talmadge</authors>
+    class Projectile : GameComponent
+    {
+        /// <summary>
+        /// The Id of this Projectile.
+        /// </summary>
+        [JsonProperty("proj")]
+        private int _projectileId;
+
+        /// <summary>
+        /// Indicates whether or not this Projectile is deactivated or not.
+        /// </summary>
+        [JsonProperty("alive")]
+        public bool Active { get; private set; }
+
+        public override Tuple<Bitmap, Rectangle> GetDrawingDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int GetId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
