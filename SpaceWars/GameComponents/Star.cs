dae11eccc468359a75dbf1a9d8290a98ed9d4a05
Jiahui Chen <jiahui.k.chen@gmail.com>
2017-11-14 01:42:10 -0700

    Star component finished.

--- SpaceWars/GameComponents/Star.cs
+++ SpaceWars/GameComponents/Star.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SpaceWars.Properties;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpaceWars
 {
-    class Star
+    /// <inheritdoc />
+    /// <summary>
+    /// Represents a Star.
+    /// </summary>
+    /// <authors>Jiahui Chen, Mitch Talmadge</authors>
+    class Star : GameComponent
     {
+        /// <summary>
+        /// The size of a star in the sprite sheet.
+        /// Four times the size of a ship.
+        /// </summary>
+        private static readonly Size StarSpriteSize = new Size(144, 176);
+
+        /// <summary>
+        /// The Id of this Star.
+        /// </summary>
+        [JsonProperty("star")]
+        private int starID;
+
+        /// <summary>
+        /// Represents this Star's mass.
+        /// </summary>
+        [JsonProperty("star")]
+        private int mass;
+
+        public override Tuple<Bitmap, Rectangle> GetDrawingDetails()
+        {
+            return new Tuple<Bitmap, Rectangle>(Resources.star,
+                new Rectangle(new Point(StarSpriteSize.Width, 0), StarSpriteSize));
+        }
+
+        /// <inheritdoc />
+        protected override int GetId()
+        {
+            return starID;
+        }
     }
 }
