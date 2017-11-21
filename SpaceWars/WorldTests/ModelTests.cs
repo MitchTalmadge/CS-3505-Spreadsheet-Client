3f4e8b8d25623e46f7ac7c4294df62dc1ab3faec
Jiahui Chen <jiahui.k.chen@gmail.com>
2017-11-20 20:08:01 -0700

    Unit testing class to test Model code made.

---
+++ SpaceWars/WorldTests/ModelTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpaceWars
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void TestShips()
+        {
+
+        }
+    }
+}
---
+++ SpaceWars/WorldTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WorldTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WorldTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4005b5b-58cb-47f0-b8e7-e57d8f0b9ac8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
