18580c5f271f341f0e3b9df2790d91c43e3e8d0b
Mitch Talmadge <u1031378@utah.edu>
2017-11-20 20:50:41 -0700

    Add logging capabilities

--- SpaceWars/Server/Program.cs
+++ SpaceWars/Server/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace SpaceWars
+﻿using System;
+using NLog;
+
+namespace SpaceWars
 {
     /// <summary>
     /// The SpaceWars Server Program.
@@ -8,8 +11,30 @@
     /// <authors>Jiahui Chen, Mitch Talmadge</authors>
     internal class Program
     {
+        /// <summary>
+        /// The logger for this program.
+        /// </summary>
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// The server controller instance attached to this view.
+        /// </summary>
+        private static readonly SpaceWarsServer Server = new SpaceWarsServer();
+
+        /// <summary>
+        /// Initializes this static console window.
+        /// The console is a singleton, and does not need state; 
+        /// therefore every part of it remains static.
+        /// </summary>
         private static void Main(string[] args)
         {
+            Logger.Log(LogLevel.Info, "Server Started Successfully.");
+
+            // Prevent closing of console.
+            while (true)
+            {
+                Console.Read();
+            }
         }
     }
 }
