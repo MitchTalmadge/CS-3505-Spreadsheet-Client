b85dfd26e62a83eb28acbc9638e9cdb8f006f858
Mitch Talmadge <u1031378@utah.edu>
2017-11-20 20:05:25 -0700

    Move Server project, rename View project to Client

--- SpaceWars/Server/Program.cs
+++ SpaceWars/Server/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Server
+﻿namespace SpaceWars
 {
-    class Program
+    /// <summary>
+    /// The SpaceWars Server Program.
+    /// This class is only the "view" of the server; the console.
+    /// The controller logic is in a separate project.
+    /// </summary>
+    /// <authors>Jiahui Chen, Mitch Talmadge</authors>
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
