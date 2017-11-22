836c46d6d620d8a9237504b3a17f55b0bc8d24a0
Mitch Talmadge <u1031378@utah.edu>
2017-11-22 01:31:24 -0700

    Move ClientConnectionState to TcpState to allow stopping of TcpListener.

--- SpaceWars/Networking/ServerNetworking.cs
+++ SpaceWars/Networking/ServerNetworking.cs
@@ -12,28 +12,32 @@ namespace Networking
     public class ServerNetworking : AbstractNetworking
     {
         /// <summary>
-        /// Waits for a client to connect to this server.
+        /// Starts an internal loop that will continually accept connections from clients until stopped via the returned TcpState.
         /// </summary>
         /// <param name="established">The callback for when a connection to a client has been established.</param>
         /// <param name="failed">The callback for when a connection to a client has failed.</param>
         /// <param name="dataReceived">The callback for when data is received from the new client.</param>
-        public void AwaitClientConnection(
+        /// <returns>A TcpState, which can be used to stop accepting connections from clients.</returns>
+        public TcpState AwaitClientConnections(
             ConnectionEstablished established,
             ConnectionFailed failed,
             DataReceived dataReceived)
         {
             var listener = new TcpListener(IPAddress.Loopback, 11000);
-            var clientConnectionState = new ClientConnectionState(listener, established, failed, dataReceived);
+            var tcpState = new TcpState(listener, established, failed, dataReceived);
 
             // Start accepting the socket from the client.
             try
             {
-                listener.BeginAcceptSocket(AcceptClientSocket, clientConnectionState);
+                listener.Start();
+                listener.BeginAcceptSocket(AcceptClientSocket, tcpState);
             }
             catch (Exception e)
             {
                 failed(e.Message);
             }
+
+            return tcpState;
         }
 
         /// <summary>
@@ -42,64 +46,34 @@ namespace Networking
         /// <param name="asyncResult">Contains the client connection state.</param>
         private static void AcceptClientSocket(IAsyncResult asyncResult)
         {
-            var clientConnectionState = (ClientConnectionState) asyncResult.AsyncState;
-            var socket = clientConnectionState.TcpListener.AcceptSocket();
+            var tcpState = (TcpState) asyncResult.AsyncState;
+
+            var socket = tcpState.TcpListener.EndAcceptSocket(asyncResult);
 
             // Create a socket state from the connection.
             var socketState = new SocketState(
-                socket, 
-                clientConnectionState.ConnectionEstablished,
-                clientConnectionState.ConnectionFailed,
-                clientConnectionState.DataReceived);
+                socket,
+                tcpState.ConnectionEstablished,
+                tcpState.ConnectionFailed,
+                tcpState.DataReceived);
+
+            tcpState.TcpListener.Stop();
 
             // Notify callback of connection established.
             socketState.ConnectionEstablished(socketState);
-        }
 
-        /// <summary>
-        /// Contains the state of a connection-in-progress from a client,
-        /// including callbacks for the server and the TCP Listener being used to connect.
-        /// </summary>
-        /// <authors>Jiahui Chen, Mitch Talmadge</authors>
-        private class ClientConnectionState
-        {
-            /// <summary>
-            /// The TCP Listener that is being used to connect to the client.
-            /// </summary>
-            public TcpListener TcpListener { get; }
+            // Exit immediately if we are not supposed to accept any more connections.
+            if (tcpState.Stopped)
+                return;
 
-            /// <summary>
-            /// The connection established callback.
-            /// </summary>
-            public ConnectionEstablished ConnectionEstablished { get; }
-
-            /// <summary>
-            /// The connection failed callback.
-            /// </summary>
-            public ConnectionFailed ConnectionFailed { get; }
-
-            /// <summary>
-            /// The data received callback.
-            /// </summary>
-            public DataReceived DataReceived { get; }
-
-            /// <summary>
-            /// Creates a client connection state with the given TCP Listener and callbacks.
-            /// </summary>
-            /// <param name="tcpListener">The TCP Listener that is being used to connect to the client.</param>
-            /// <param name="established">The connection established callback.</param>
-            /// <param name="failed">The connection failed callback.</param>
-            /// <param name="dataReceived">The data received callback.</param>
-            public ClientConnectionState(
-                TcpListener tcpListener,
-                ConnectionEstablished established,
-                ConnectionFailed failed,
-                DataReceived dataReceived)
+            // Accept another connection.
+            try
             {
-                TcpListener = tcpListener;
-                ConnectionEstablished = established;
-                ConnectionFailed = failed;
-                DataReceived = dataReceived;
+                tcpState.TcpListener.BeginAcceptSocket(AcceptClientSocket, tcpState);
+            }
+            catch (Exception e)
+            {
+                tcpState.ConnectionFailed(e.Message);
             }
         }
     }
---
+++ SpaceWars/Networking/TcpState.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking
+{
+    /// <summary>
+    /// Contains a TcpListener instance that is used to accept sockets from clients.
+    /// Includes connection and data callbacks, which are passed to a SocketState once 
+    /// a socket has been accepted.
+    /// </summary>
+    /// <authors>Jiahui Chen, Mitch Talmadge</authors>
+    public class TcpState
+    {
+        /// <summary>
+        /// The TCP Listener that is being used to connect to the client.
+        /// </summary>
+        internal TcpListener TcpListener { get; }
+
+        /// <summary>
+        /// Determines if this TcpState has been stopped.
+        /// If true, no more connections should be accepted.
+        /// </summary>
+        internal bool Stopped { get; private set; }
+
+        /// <summary>
+        /// The connection established callback.
+        /// </summary>
+        internal AbstractNetworking.ConnectionEstablished ConnectionEstablished { get; }
+
+        /// <summary>
+        /// The connection failed callback.
+        /// </summary>
+        internal AbstractNetworking.ConnectionFailed ConnectionFailed { get; }
+
+        /// <summary>
+        /// The data received callback.
+        /// </summary>
+        internal AbstractNetworking.DataReceived DataReceived { get; }
+
+        /// <summary>
+        /// Creates a client connection state with the given TCP Listener and callbacks.
+        /// </summary>
+        /// <param name="tcpListener">The TCP Listener that is being used to connect to the client.</param>
+        /// <param name="established">The connection established callback.</param>
+        /// <param name="failed">The connection failed callback.</param>
+        /// <param name="dataReceived">The data received callback.</param>
+        internal TcpState(
+            TcpListener tcpListener,
+            AbstractNetworking.ConnectionEstablished established,
+            AbstractNetworking.ConnectionFailed failed,
+            AbstractNetworking.DataReceived dataReceived)
+        {
+            TcpListener = tcpListener;
+            ConnectionEstablished = established;
+            ConnectionFailed = failed;
+            DataReceived = dataReceived;
+        }
+
+        /// <summary>
+        /// Stops accepting client connections on this TcpState.
+        /// This instance should be discarded after calling this method.
+        /// </summary>
+        public void StopAcceptingClientConnections()
+        {
+            try
+            {
+                Stopped = true;
+                TcpListener.Stop();
+            }
+            catch (SocketException e)
+            {
+                ConnectionFailed(e.Message);
+            }
+        }
+    }
+}
