7afd11822c81d750055c7af1555ff1d44b97804d
Mitch Talmadge <u1031378@utah.edu>
2017-11-30 18:47:22 -0700

    Add method structure for game loop

--- SpaceWars/ServerController/SpaceWarsServer.GameLoop.cs
+++ SpaceWars/ServerController/SpaceWarsServer.GameLoop.cs
@@ -37,7 +37,84 @@ namespace SpaceWars
         /// </summary>
         private void OnTick()
         {
-            Console.Out.WriteLine("Loop!");
+            // Cleaning
+            CleanupProjectiles();
+
+            // Spawning
+            SpawnShips();
+            SpawnProjectiles();
+
+            // Motion
+            ComputeShipMotion();
+            ComputeProjectileMotion();
+            ComputeWrapping();
+
+            // Collision
+            ComputeCollision();
+
+            // Notify listeners of world updated
+            // TODO
         }
+
+        /// <summary>
+        /// Cleans dead projectiles.
+        /// </summary>
+        private void CleanupProjectiles()
+        {
+
+        }
+
+        /// <summary>
+        /// Spawns any new or respawning ships in an empty location
+        /// </summary>
+        private void SpawnShips()
+        {
+
+        }
+
+        /// <summary>
+        /// Spawns any new projectils due to firing commands.
+        /// </summary>
+        private void SpawnProjectiles()
+        {
+            
+        }
+
+        /// <summary>
+        /// Computes how ships should move, based on gravity, thrust commands, and turning commands.
+        /// </summary>
+        private void ComputeShipMotion()
+        {
+            
+        }
+
+        /// <summary>
+        /// Computes how projectiles should move, based only on a constant velocity.
+        /// If a projectile goes out of bounds, mark it as "dead."
+        /// </summary>
+        private void ComputeProjectileMotion()
+        {
+
+        }
+
+        /// <summary>
+        /// Wraps any out-of-bounds ships to the other side of the world.
+        /// </summary>
+        private void ComputeWrapping()
+        {
+            
+        }
+
+        /// <summary>
+        /// Determines where and when projectiles collide with ships and stars.
+        /// Decreases the health of ships where needed.
+        /// Marks collided projectiles as "dead."
+        /// If a ship dies as result of the collision, a point is added to the ship who owned the projectile.
+        /// </summary>
+        private void ComputeCollision()
+        {
+            
+        }
+
     }
 }
