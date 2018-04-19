7a03696cd351fc2e1aa69ba2cc4636bc96085111
Jiahui Chen <jiahui.k.chen@gmail.com>
2018-04-18 20:41:14 -0600

    Setup for the Focus and Unfocus GUI feature.

--- Client/SpreadsheetGUI/SpreadsheetForm.SpreadsheetPanel.cs
+++ Client/SpreadsheetGUI/SpreadsheetForm.SpreadsheetPanel.cs
@@ -48,6 +48,24 @@ namespace SpreadsheetGUI
         }
 
         /// <summary>
+        /// Edits GUI of spreadsheet panel to display cells being edited by other clients on the Server. 
+        /// </summary>
+        /// <param name="cell"></param>
+        private void SpreadsheetPanel_Focus(string cell)
+        {
+
+        }
+
+        /// <summary>
+        /// Edits GUI of spreadsheet panel to stop displaying cells no longer being
+        /// edited by other clients on the Server. 
+        /// </summary>
+        private void SpreadsheetPanel_Unfocus()
+        {
+
+        }
+
+        /// <summary>
         /// Called when enter is pressed while cell editor text box is selected.
         /// </summary>
         /// <param name="sender">The Spreadsheet Panel containing the cell.</param>
